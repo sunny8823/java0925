f01a5c4698aef60457ec1085df6735eb534ac059
sunny8823 <bloodmoon5210@gmail.com>
2017-09-25 15:06:21 +0800

    frist commit

--- src/gittest.java
+++ src/gittest.java
@@ -1,5 +1,4 @@
-public class gittest {
-    public static void main(String arg[]){
-        System.out.println("我的第一個java");
-    }
+public class gittest { public static void main(String arg[]){
+    System.out.println("我的第一個java");
+}
 }
